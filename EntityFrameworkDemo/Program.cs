c8a5c5a32cfbd4960f9dfea75075d593efdafe91
sametion <sametacikk@gmail.com>
2024-07-18 16:20:44 +0300

    efdemo commit
    ef configurations

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GetAll();
+            GetAllByCategoryId(1);
+
+        }
+
+        private static void GetAll()
+        {
+            NorthwindContext context = new NorthwindContext();
+            foreach (var item in context.Products)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+        private static void GetAllByCategoryId(int categoryId)
+        {
+            NorthwindContext context = new NorthwindContext();
+            var result= context.Products.Where(p=> p.CategoryId == categoryId);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+    }
+}
